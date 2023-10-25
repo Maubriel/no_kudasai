8aab97fd7e793114c4f79413bc8738da759695d3
Mauricio <maubriel.py@gmail.com>
2023-10-25 16:01:00 -0300

    Extension para firefox

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.getElementById("Kudasai").style = "display:none";
+// document.getElementById("disqus_thread").style = "display:none";
+
+var seccion_comentarios = document.querySelector("div.WdgtCn:not(.DwsldCn)")
+seccion_comentarios.style = "display:none"
+
+var seccion_compartir = document.querySelector("div.ShrCnB.fa-share-alt")
+seccion_compartir.style = "display:none"
+
+var seccion_noticias = document.querySelector("div.CpCnC")
+seccion_noticias.style = "display:none"
